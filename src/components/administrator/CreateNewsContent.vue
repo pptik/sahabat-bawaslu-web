8b2b80973dd0bfafe00bd0e3702667c233f73b10
fiyyanputra <hafiyyan.jtk10@gmail.com>
2018-08-20 12:49:24 +0700

    added challenge

--- src/components/administrator/CreateNewsContent.vue
+++ src/components/administrator/CreateNewsContent.vue
@@ -22,9 +22,26 @@
      <div class="field">
        <label>Konten</label>
        <vue-editor id="editor" useCustomImageHandler @imageAdded="handleImageAdded" v-model="content">
-    </vue-editor>
-
+       </vue-editor>
      </div>
+     <div class="field">
+       <label>Hashtag</label>
+       <input type="text" placeholder="Hashtag pisahkan dengan koma tanpa #" v-model="hashtag"/>
+     </div>
+     <div class="ui checkbox">
+      <input type="checkbox" id="checkbox" v-model="isChallenge">
+      <label for="checkbox">Check bila challenge</label>
+     </div>
+    <div id="challenge" class="field" style="display: none">    
+     <div class="field">
+       <label>Poin Challenge</label>
+       <input type="text" placeholder="Poin Challenge" v-model="poin"/>
+     </div>
+     <div class="field">
+       <label>Kadaluarsa Challenge</label>
+       <input type="text" id="datepicker" placeholder="Expire" v-model="expire" name="expire" />
+     </div>
+    </div>
       <div class="container">
           <button v-on:click.prevent="kirimNews" type="button" class="small ui blue button">Submit</button>
         </div>
@@ -44,10 +61,15 @@
         hasError:false,
         loading:false,
         hasMessage:false,
+        isChallenge:false,
         messageText:'',
         title:'',
         content:'',
-        desc:''
+        desc:'',
+        poin: '',
+        expire: '',
+        hashtag: '',
+        hashtagList: []
       }
     },
     components: {
@@ -57,11 +79,29 @@
 
     },
     mounted() {
+      var vm = this
       this.reset();
+      //datepicker
+      $( function() {
+        $( "#datepicker" ).datepicker({ 
+          dateFormat: 'dd/mm/yy',
+          onSelect:function(selectedDate, datePicker) {            
+            vm.expire = selectedDate;
+          }
+        });
+      } )
+      $("#checkbox").click(function() {
+        if($("#checkbox").is(':checked'))
+          $("#challenge").css({"display":""});  // checked
+        else
+          $("#challenge").css({"display":"none"});  // unchecked
+      });
+
+      
     },
     methods: {
       kirimNews(){
-        this.loading=true;
+        //this.loading=true;
         let errorCheck=false;
         this.hasMessage=false;
         this.hasError=false;
@@ -80,19 +120,58 @@
           this.hasError=true;
           this.errorText='Silahkan Isi Deskripsi Singkat';
         }
+        if(this.hashtag===""){
+            // errorCheck=true;
+            // this.hasError=true;
+            // this.errorText='Silahkan Isi Hashtag';
+            this.hashtagList.push('');
+          }else{ 
+            var string = this.hashtag;
+            var split=string.split(",");
+              for(var i=0; i<split.length; i++){
+                this.hashtagList.push(split[i]);
+              } 
+          }
+        if(this.isChallenge===true){
+          if(this.poin===""){
+            errorCheck=true;
+            this.hasError=true;
+            this.errorText='Silahkan Isi Poin Challange';
+          }
+          if(this.expire===""){
+            errorCheck=true;
+            this.hasError=true;
+            this.errorText='Silahkan Isi Kadaluarsa Challange';
+          }
+        }
 
         if(!errorCheck){
-          this.loading=true;
-          this.$http.post(restAPI.createnews,{
+          let news = {
               Title: this.title,
               Content:this.content,
-            Desc:this.desc
-            },{
+              Desc:this.desc,
+              Tags: this.hashtagList
+            }
+
+          if(this.isChallenge) {            
+            news = {
+              Title: this.title,
+              Content:this.content,
+              Desc:this.desc,
+              IsChallenge: "true",
+              ChallengePoin: this.poin,
+              ChallengeExpire: this.expire,
+              Tags: this.hashtagList
+            }
+          } 
+          
+          //alert(JSON.stringify(news));
+          this.loading=true;
+          this.$http.post(restAPI.createnews,news,{
               headers:{
                 access_token:this.$session.get('access_token')
               },
-            }
-          ).then(function (data) {
+            }).then(function (data) {
             console.log(data);
             if(data.body.success === true){
               this.hasMessage=true;
@@ -105,7 +184,6 @@
             this.loading=false;
           });
         }
-
       },
       handleImageAdded: function(file, Editor, cursorLocation, resetUploader) {
         let formData = new FormData();
@@ -137,9 +215,13 @@
       reset() {
         // reset form to initial state
         this.title='';
-          this.content='';
-          this.desc='';
+        this.content='';
+        this.desc='';
         this.hasError = false;
+        this.isChallenge=false;
+        this.poin='';
+        this.expire='';
+        this.hashtag='';
       }
     }
   }
